a42e2751da07fc35936785f29d7b0a1d6406885c
Matthias Mintert <matthias@mintert.net>
2019-03-05 14:48:52 +0100

    externalpipe command for URLs

--- config.def.h
+++ config.def.h
@@ -206,6 +206,10 @@ MouseKey mkeys[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
+static char *openurlcmd[] = { "/bin/sh", "-c",
+		"xurls | uniq | dmenu -l 10 | xargs -r xdg-open",
+		"externalpipe", NULL };
+
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
@@ -222,6 +226,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,				XK_U,			externalpipe,	{.v = openurlcmd} },
 };
 
 /*
